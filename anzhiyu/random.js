efcb4ff10207bbc643f075e75625d1665b6d99ab
xiaohuihui <904036925@qq.com>
2024-08-30 20:22:15 +0800

    Site updated: 2024-08-30 20:22:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/08/30/hello-world/","2024/08/30/myblog/","2024/08/30/webArticle/myblog/","2024/08/30/webArticle/hello-world/"];function toRandomPost(){
+var posts=["2024/08/30/myblog/","2024/08/30/webArticle/hello-world/","2024/08/30/webArticle/myblog/","2024/08/30/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
